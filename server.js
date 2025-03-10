20702b1045b8b63c66a4cb2a89b99f61f6fb13a5
abl00mfield <amanda.bloomfield@gmail.com>
2025-03-10 11:47:33 -0600

    wip: add server boilerplate

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
 const dotenv = require("dotenv");
 const express = require("express");
-const app = express();
 const mongoose = require("mongoose");
 const methodOverride = require("method-override");
 const morgan = require("morgan");
 
+//initialize express app
+const app = express();
+
+//configure settings
 dotenv.config();
 // Set the port from environment variable or default to 3000
 const port = process.env.PORT ? process.env.PORT : "3000";
 
+//connect to mongo DB
 mongoose.connect(process.env.MONGODB_URI);
 mongoose.connection.on("connected", () => {
   console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
@@ -21,6 +25,9 @@ app.use(methodOverride("_method"));
 // Morgan for logging HTTP requests
 app.use(morgan("dev"));
 
+//mount routes
+
+//tell the app to listen
 app.listen(port, () => {
   console.log(`The express app is ready on port ${port}!`);
 });
