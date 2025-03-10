72151d75af5316f7f5a5e441f1ce2164caba43f9
abl00mfield <amanda.bloomfield@gmail.com>
2025-03-10 15:38:26 -0600

    wip: sign up functionality created

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,10 +1,32 @@
 const express = require("express");
 const router = express.Router();
+const User = require("../models/user");
+const bcrypt = require("bcrypt");
 
 //router object is similar to app object in server js
 //however, it only has router functionality
 router.get("/sign-up", (req, res) => {
   res.render("auth/sign-up.ejs");
+});
+
+router.post("/sign-up", async (req, res) => {
+  //check if user exists
+  const userInDatabase = await User.findOne({ username: req.body.username });
+  if (userInDatabase) {
+    return res.send("Username already taken");
+  }
+  //check if password and confirmPassword are the same
+  if (req.body.password != req.body.confirmPassword) {
+    return res.send("Password and confirm password must match");
+  }
+
+  const hashedPassword = bcrypt.hashSync(req.body.password, 10);
+  req.body.password = hashedPassword;
+  //create user in database
+  const user = await User.create(req.body);
+  res.send(`Thanks for signing up ${user.username}`);
+
+  //   res.redirect();
 });
 
 module.exports = router;
